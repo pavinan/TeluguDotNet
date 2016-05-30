80f13576a01c076060a274f56020be13251fb1f1
pavinan <pavinan@users.noreply.github.com>
2016-05-30 17:03:54 +0530

    test project

---
+++ తెలుగు/తెలుగు.టెస్ట్/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace తెలుగు.టెస్ట్
+{
+    class Program
+    {
+        static void Main(string[] args) => ప్రోగ్రామ్.మెయ్న్(args);
+    }
+}
---
+++ తెలుగు/తెలుగు.టెస్ట్/ప్రోగ్రామ్.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace తెలుగు.టెస్ట్
+{
+    public class ప్రోగ్రామ్
+    {
+        public static void మెయ్న్(string[] ఆర్గ్స్)
+        {
+
+        }
+    }
+}

9249109843928ff560cc2ccdd190ba740a4337f8
pavinan <pavinan@users.noreply.github.com>
2016-05-30 16:56:41 +0530

    First Commit

---
+++ తెలుగు/తెలుగు/సిస్టమ్.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace సిస్టమ్
+{
+    public class కన్సోల్
+    {
+        public void రైట్లైన్() => Console.WriteLine();
+
+        public void రైట్లైన్(bool వాల్యు) => Console.WriteLine(వాల్యు);
+
+        public void రైట్లైన్(float వాల్యు) => Console.WriteLine(వాల్యు);
+
+        public void రైట్లైన్(int వాల్యు) => Console.WriteLine(వాల్యు);
+        public void రైట్లైన్(uint వాల్యు) => Console.WriteLine(వాల్యు);
+
+        public void రైట్లైన్(long వాల్యు) => Console.WriteLine(వాల్యు);
+
+        public void రైట్లైన్(ulong వాల్యు) => Console.WriteLine(వాల్యు);
+
+        public void రైట్లైన్(object వాల్యు) => Console.WriteLine(వాల్యు);
+
+        public void రైట్లైన్(string వాల్యు) => Console.WriteLine(వాల్యు);
+
+        public void రైట్లైన్(double వాల్యు) => Console.WriteLine(వాల్యు);
+
+        public void రైట్లైన్(decimal వాల్యు) => Console.WriteLine(వాల్యు);
+
+        public void రైట్లైన్(char[] వాల్యు) => Console.WriteLine(వాల్యు);
+
+        public void రైట్లైన్(string ఫార్మాట్, params object[] ఆర్గ్) => Console.WriteLine(ఫార్మాట్, ఆర్గ్);
+
+        public void రైట్లైన్(string ఫార్మాట్, object ఆర్గ్0) => Console.WriteLine(ఫార్మాట్, ఆర్గ్0);
+
+        public void రైట్లైన్(char[] బఫర్, int ఇన్డెక్స్, int కౌంట్) => Console.WriteLine(బఫర్, ఇన్డెక్స్, కౌంట్);
+
+        public void రైట్లైన్(string ఫార్మాట్, object ఆర్గ్0, object ఆర్గ్1) => Console.WriteLine(ఫార్మాట్, ఆర్గ్0, ఆర్గ్1);
+
+        public void రైట్లైన్(string ఫార్మాట్, object ఆర్గ్0, object ఆర్గ్1, object ఆర్గ్2) => Console.WriteLine(ఫార్మాట్, ఆర్గ్0, ఆర్గ్1, ఆర్గ్2);
+
+        public void రైట్లైన్(string ఫార్మాట్, object ఆర్గ్0, object ఆర్గ్1, object ఆర్గ్2, object ఆర్గ్3) => Console.WriteLine(ఫార్మాట్, ఆర్గ్0, ఆర్గ్1, ఆర్గ్2, ఆర్గ్3);
+    }
+
+}
